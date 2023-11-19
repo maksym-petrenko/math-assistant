43b8abac6a19ac03221371c5f7af40a2aebd4852
Eduard Tykhoniuk <edik.tikhoniuk@gmail.com>
2023-11-20 00:20:43 +0100

    Move wolfram into separate folder

--- wolfram/helper.py
+++ wolfram/helper.py
@@ -1,19 +1,20 @@
 import urllib
-from typing import Any
+from typing import Any, Literal
 
 import requests
-
 from config import credentials
 
 base_query = f'http://api.wolframalpha.com/v2/query?appid={credentials.APP_ID}'
 
 
+POSSIBLE_FORMATS = Literal['image', 'imagemap', 'plaintext', 'MathML', 'Sound', 'wav']
+
 # returns pod
-def get_step_by_step_solution(query: str) -> Any | None:
+def get_step_by_step_solution(query: str, output_format: POSSIBLE_FORMATS) -> Any | None:
     query = 'Solve: ' + urllib.parse.quote_plus(query)
 
     pod_data = 'includepodid=Result&podstate=Step-by-step solution'
-    url = f'{base_query}&input={query}&format=image&output=json&{pod_data}'
+    url = f'{base_query}&input={query}&format={output_format}&output=json&{pod_data}'
 
     response = requests.get(url, timeout=5)
     if not response.ok:
@@ -33,5 +34,7 @@ def get_step_by_step_solution(query: str) -> Any | None:
     return None
 
 
-pod = get_step_by_step_solution('x^3 + x^2 + x = 3')
-print(pod['img']['src'])
+# returns URL to image with step by step solution
+def get_step_by_step_solution_image_only(query: str) -> str | None:
+    pod = get_step_by_step_solution(query, 'image')
+    return pod['img']['src'] if pod else None
