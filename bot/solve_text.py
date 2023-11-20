1ffe827acf13de43b75e96549a697d6ef962c80f
Eduard Tykhoniuk <edik.tikhoniuk@gmail.com>
2023-11-20 02:50:55 +0100

    Fix: use `raw_text` instead of `text` because of formating

--- bot/solve_text.py
+++ bot/solve_text.py
@@ -5,10 +5,10 @@ from wolfram.helper import get_step_by_step_solution_image_only
 from .config import bot
 
 
-@bot.on(events.NewMessage(incoming=True, func=lambda event: event.text != ''))
+@bot.on(events.NewMessage(incoming=True, func=lambda event: event.raw_text != ''))
 async def solve_text(msg: events.NewMessage) -> None:
-    url = get_step_by_step_solution_image_only(msg.text)
-    print(msg.text, '->', url)
+    url = get_step_by_step_solution_image_only(msg.raw_text)
+    print(msg.raw_text, '->', url)
 
     if url is None:
         await msg.reply('Something went wrong')
