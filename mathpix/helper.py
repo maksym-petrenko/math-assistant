249677b0eb4bd828be140c6f22edb6ebe80154dd
Eduard Tykhoniuk <edik.tikhoniuk@gmail.com>
2023-11-25 21:16:26 +0100

    Revert "fix: image transformed directly to wolfram language"
    This reverts commit c5714400126c0dad96acfec8d233af10eae793e5.

--- mathpix/helper.py
+++ mathpix/helper.py
@@ -1,10 +1,13 @@
 import json
 
+from sympy import mathematica_code
+from sympy.parsing.latex import parse_latex
+
 from helper.aiohttp_client import get_client
 
 from .config import credentials
 
-API = 'https://api.mathpix.com/v3/latex'
+API = 'https://api.mathpix.com/v3/text'
 
 async def image_to_mathematica(image: bytes) -> str:
     """Use Mathpix API to get mathematica code of math formulas on the image."""
@@ -15,14 +18,19 @@ async def image_to_mathematica(image: bytes) -> str:
         data={
             'file': image,
             'options_json': json.dumps({
-                'formats': ['wolfram'],
+                'math_inline_delimiters': ['', ''],
+                'rm_spaces': True,
             }),
         },
         headers={
             'app_id': credentials.APP_ID,
             'app_key': credentials.APP_KEY,
         },
+        timeout=5,
     )
 
     async with request as response:
-        return (await response.json())['wolfram']
+        latex = (await response.json())['text']
+
+    mathematica = mathematica_code(parse_latex(latex))
+    return mathematica.replace('Hold', '')  # TODO: find better way
