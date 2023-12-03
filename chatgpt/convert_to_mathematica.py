4732a953176eae1b68c9cf73eae341af3058ba0e
maksym-petrenko <maksym.petrenko.a@gmail.com>
2023-12-03 16:31:13 +0200

    fix: improved prompt fot chatgptAPI

--- chatgpt/convert_to_mathematica.py
+++ chatgpt/convert_to_mathematica.py
@@ -2,7 +2,11 @@ from .config import client
 
 PROMPT = """
 Your response must contain a formula only, description text and other explanations are forbidden.
+You must identify the intention of the formula's author.
+For example, if infinite sum is expressed, you should give wolfram instructions
+to find out whether it converges and if yes, to calculate the sum.
 Convert the following text to Wolfram Mathematica language
+If you can't transform this LaTeX to mathematica, you must reply with the only word: None.
 """
 
 
