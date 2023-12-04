7ea01d559c3434a4d9803e5a05c8f992e7651d88
maksym-petrenko <maksym.petrenko.a@gmail.com>
2023-12-04 20:37:08 +0200

    fix: chatgpt prompt

--- chatgpt/convert_to_mathematica.py
+++ chatgpt/convert_to_mathematica.py
@@ -5,8 +5,11 @@ Your response must contain a formula only, description text and other explanatio
 You must identify the intention of the formula's author.
 For example, if infinite sum is expressed, you should give wolfram instructions
 to find out whether it converges and if yes, to calculate the sum.
-Convert the following text to Wolfram Mathematica language
-If you can't transform this LaTeX to mathematica, you must reply with the only word: "None".
+Convert the following text to Wolfram Mathematica language.
+
+You must answer with it only. For example,
+Input: 16 x^{3}-18 x^{2}+4 x-21=0
+Output: Solve[16 x^3 - 18 x^2 + 4 x - 21 == 0, x]
 """
 
 
