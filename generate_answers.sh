36778058a3204036a20ec4f4881255b62fd51662
Eduard Tykhoniuk <edik.tikhoniuk@gmail.com>
2023-12-05 19:23:38 +0100

    Add script for generating answers

---
+++ generate_answers.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker compose run -v $(pwd)/tests/data:/bot/tests/data bot python -m tests.data.generate_answers
---
+++ tests/data/generate_answers.py
@@ -0,0 +1,39 @@
+import asyncio
+import json
+from pathlib import Path
+
+from chatgpt.convert_to_mathematica import convert
+from helper.aiohttp_client import stop as stop_client
+from helper.main_handler import main_handler
+from wolfram.helper import get_step_by_step_solution
+
+data_path = Path(__file__).parent
+
+# only generates if there no answer yet
+async def generate_answer(test: dict) -> str:
+    if 'answer' in test:
+        return test['answer']
+
+    question = test['question']
+    print('generating for:', question)
+
+    converted = await convert(question)
+    pods = await get_step_by_step_solution(converted, 'image')
+
+    return pods[0]['img']['alt']
+
+
+async def main() -> None:
+    tests = []
+    with open(data_path / 'text.json') as text_data:  # noqa: ASYNC101
+        tests = json.load(text_data)
+
+    answers = await asyncio.gather(*[generate_answer(test) for test in tests])
+    for test, answer in zip(tests, answers, strict=True):
+        test['answer'] = answer
+
+    with open(data_path / 'text.json', 'w') as text_data:  # noqa: ASYNC101
+        json.dump(tests, text_data, indent=4)
+
+if __name__ == '__main__':
+    main_handler(main, None, stop_client)
