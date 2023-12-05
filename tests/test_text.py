9713570065c88cf51e9a2ea6bfea872118e70ce4
Eduard Tykhoniuk <edik.tikhoniuk@gmail.com>
2023-12-05 18:49:25 +0100

    Use json for tests' data, fix tests

---
+++ tests/test_text.py
@@ -0,0 +1,22 @@
+import json
+
+import pytest
+
+from chatgpt.convert_to_mathematica import convert
+from wolfram.helper import get_step_by_step_solution
+
+with open('tests/data/text.json') as data:
+    tests: list = json.load(data)
+
+test_tuples = [(test['question'], test['answer']) for test in tests]
+
+
+@pytest.mark.parametrize(('question', 'answer'), test_tuples)
+@pytest.mark.asyncio()
+async def test_text2answer(question: str, answer: str):
+    """Test ChatGPT + Wolfram performance on input strings."""
+
+    converted = await convert(question)
+    pods = await get_step_by_step_solution(converted, 'image')
+
+    assert pods[0]['title'] == answer
