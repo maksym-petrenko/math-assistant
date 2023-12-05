7803737d306c6cebe282edcd78dbc831df1e3b28
Eduard Tykhoniuk <edik.tikhoniuk@gmail.com>
2023-12-05 23:34:54 +0100

    Fix typing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,12 @@
+from collections.abc import AsyncGenerator
+
 import pytest
 
 from helper.aiohttp_client import stop
 
 
 @pytest.fixture(autouse=True)
-async def _cleanup() -> None:
+async def _cleanup() -> AsyncGenerator[None, None]:
     try:
         yield
     finally:
