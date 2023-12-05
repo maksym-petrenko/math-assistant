d6b5ef2e8142ecf9625e5f0f3c0a5e67241dfc95
maksym-petrenko <maksym.petrenko.a@gmail.com>
2023-12-05 19:40:55 +0200

    fix: PROMPT improved, more examples provided

--- chatgpt/convert_to_mathematica.py
+++ chatgpt/convert_to_mathematica.py
@@ -1,15 +1,25 @@
 from .config import client
 
-PROMPT = """
+PROMPT = r"""
 Your response must contain a formula only, description text and other explanations are forbidden.
 You must identify the intention of the formula's author.
 For example, if infinite sum is expressed, you should give wolfram instructions
 to find out whether it converges and if yes, to calculate the sum.
 Convert the following text to Wolfram Mathematica language.
 
-You must answer with it only. For example,
-Input: 16 x^{3}-18 x^{2}+4 x-21=0
-Output: Solve[16 x^3 - 18 x^2 + 4 x - 21 == 0, x]
+You must answer with the formula only. Here are some examples:
+
+Input: x^{3}-12 x^{2}+19 x+8=0
+Output: Solve[x^3 - 12 x^2 + 19 x + 8 == 0, x]
+
+Input: \operatorname{Rank}\left[\begin{array}{ccc}1 & 1 & 1 \\ 1 & 3 & -4 \\ 1 & 7 & 1 \\ 3 & 9 & 11\end{array}\right]
+Output: MatrixRank[{{1, 1, 1}, {1, 3, -4}, {1, 7, 1}, {3, 9, 11}}]
+
+Input: \sum_{n=1}^{\infty}\left(\cos \frac{1}{n} \sin \frac{1}{n}\right).
+Output: Sum[Cos[1/n] Sin[1/n], {n, 1, Infinity}]
+
+Input: \sum_{x=1}^{12} x^{k}=k
+Output: Sum[x^k, {x, 1, 12}] = k
 """
 
 
