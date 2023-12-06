04181ca5799a4b3225e53769fbcb9ddd3a81110f
maksym-petrenko <maksym.petrenko.a@gmail.com>
2023-12-06 02:05:06 +0200

    small improvements to the PROMPT

--- chatgpt/convert_to_mathematica.py
+++ chatgpt/convert_to_mathematica.py
@@ -1,6 +1,8 @@
 from .config import client
 
 PROMPT = r"""
+If the formula is already in mathematica, return it only.
+
 Your response must contain a formula only, description text and other explanations are forbidden.
 You must identify the intention of the formula's author.
 For example, if infinite sum is expressed, you should give wolfram instructions
@@ -8,6 +10,9 @@ to find out whether it converges and if yes, to calculate the sum.
 If possible, apply Solve to it, but only if it makes sense.
 Convert the following text to Wolfram Mathematica language.
 Please, make sure it can be processed by Wolfram.
+
+In case there is a function without any equations, please, give raw output so that Wolfram can provide general information about it.
+Make sure that all of the variables and functions are spelled correctly and don't contain unnecessary ^ or _
 
 Only if case the input is invalid and doesn't contain any formulas, you have to respond with the only word "None".
 You must answer with the formula only. Here are some examples:
@@ -23,10 +28,15 @@ Output: Sum[Cos[1/n] Sin[1/n], {n, 1, Infinity}]
 
 Input: \sum_{x=1}^{12} x^{k}=k
 Output: Sum[x^k, {x, 1, 12}] = k
+
+Input: f(x)=\left\{\begin{array}{ll}\sin(x) & \text { if } x=0 \\ 2 x & \text { if } x \neq \cos(x)\end{array}\right
+Output: f[x] := Piecewise[{{Sin(x), x = 0}, {Cos(x), Unequal[x, 0]}}]
 """
 
 
 async def convert(latex: str) -> str | None:
+    print(latex)
+
     messages = [{'role': 'system', 'content': PROMPT}, {'role': 'user', 'content': latex}]
     response = await client.chat.completions.create(
         model='gpt-3.5-turbo',
@@ -35,6 +45,8 @@ async def convert(latex: str) -> str | None:
     )
 
     answer = response.choices[0].message.content
+
+    print(answer)
     if answer == 'None':
         return None
     return answer
