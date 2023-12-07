84aebe3401077e16b7dcdc39e23f728bdd00dc4f
Eduard Tykhoniuk <edik.tikhoniuk@gmail.com>
2023-12-07 03:07:01 +0100

    Use `plaintext` instead of `image`

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -6,7 +6,7 @@ def serealize_pods(pods: list | None) -> list[str] | None:
     if pods is None:
         return None
 
-    return [pod['img']['alt'] for pod in pods]
+    return [pod['plaintext'] for pod in pods]
 
 
 async def question2pods(question: str) -> list[str] | None:
@@ -14,5 +14,5 @@ async def question2pods(question: str) -> list[str] | None:
     if converted is None:
         return None
 
-    pods = await get_step_by_step_solution(converted, 'image')
+    pods = await get_step_by_step_solution(converted, 'plaintext')
     return serealize_pods(pods)
