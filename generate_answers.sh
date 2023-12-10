91effcbcd594081a61338d6025cf3bedb3569b70
Eduard Tykhoniuk <edik.tikhoniuk@gmail.com>
2023-12-10 16:31:15 +0100

    Fix scripts

--- generate_answers.sh
+++ generate_answers.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker compose run -v $(pwd):/bot bot python -m tests.data.generate_answers $*
+docker compose -f docker-compose-dev.yml run -v $(pwd):/bot bot python -m tests.data.generate_answers $*
