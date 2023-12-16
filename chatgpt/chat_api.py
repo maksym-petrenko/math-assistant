109d60b64ac92daa387942eedbca19f385e75bf3
maksym-petrenko <maksym.petrenko.a@gmail.com>
2023-12-16 18:07:11 +0200

    add minor improvements to chat_api

--- chatgpt/chat_api.py
+++ chatgpt/chat_api.py
@@ -1,34 +1,46 @@
 from typing import Literal
 
+from pydantic import BaseModel
+
 from .config import SEED, client
 from .helper import read_prompt
 
-prompt2file = {
+PROBLEM_TYPES = Literal['Classify', 'Solve', 'Wolfram']
+MODELS = Literal['gpt-3.5-turbo', 'gpt-4']
+
+PROMPT2INFO = {
     'Classify': {
         'prompt': read_prompt('classifier'),
         'model': 'gpt-3.5-turbo',
-        },
+    },
     'Wolfram': {
         'prompt': read_prompt('mathematica'),
         'model': 'gpt-3.5-turbo',
-        },
+    },
     'Solve': {
         'prompt': read_prompt('gpt_solver'),
         'model': 'gpt-4',
-        },
+    },
 }
 
 
-async def gpt(message: str, request_type: Literal['Classify', 'Solve', 'Wolfram']) -> str | None:
+class PromptInfo(BaseModel):
+
+    prompt: str
+    model: MODELS
+
+
+async def gpt(message: str, request_type: PROBLEM_TYPES) -> str | None:
     """Interact with ChatGPT API."""
 
     #  get prompt and model suitable for the task type
-    prompt, model = prompt2file[request_type].values()
+    info = PromptInfo(prompt=PROMPT2INFO[request_type]['prompt'],
+                      model=PROMPT2INFO[request_type]['model'])
 
-    messages = [{'role': 'system', 'content': prompt}, {'role': 'user', 'content': message}]
+    messages = [{'role': 'system', 'content': info.prompt}, {'role': 'user', 'content': message}]
 
     response = await client.chat.completions.create(
-        model=model,
+        model=info.model,
         messages=messages,  # type: ignore[arg-type]
         seed=SEED,
         temperature=0,
